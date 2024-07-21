2563ec3dafb7d7f70245a167302548ac37c0776b
Dennis Dumin <dumindennis@gmail.com>
2024-07-21 20:28:44 +0200

    responsive contact & addtask

--- js/contact.js
+++ js/contact.js
@@ -376,29 +376,29 @@ async function testFetch() {
     console.log(responseAsJson);
 }
 
-function checkUserMaxWidth() {
-    const mobileWidth = 800;
+// function checkUserMaxWidth() {
+//     const mobileWidth = 800;
+// 
+//     if (window.matchMedia(`(max-width: ${mobileWidth}px)`).matches) {
+//         document.getElementById('mobileContactReturn').classList.remove('displayNone');
+//         document.getElementById('contactList').classList.add('displayNone');
+//         document.getElementById('contentSection').classList.remove('dNone');
+//     } 
+// }
 
-    if (window.matchMedia(`(max-width: ${mobileWidth}px)`).matches) {
-        document.getElementById('mobileContactReturn').classList.remove('displayNone');
-        document.getElementById('contactList').classList.add('displayNone');
-        document.getElementById('contentSection').classList.remove('dNone');
-    } 
-}
+// window.addEventListener('resize', hideMobileAssets)
 
-window.addEventListener('resize', hideMobileAssets)
-
-function hideMobileAssets() {
-    if (window.innerWidth > 800) {
-        document.getElementById('mobileContactReturn').classList.add('displayNone');
-        document.getElementById('return_mobilePopUp').classList.add('displayNone');
-        document.getElementById('return_editMobilePopUp').classList.add('displayNone');
-    } else if (window.innerWidth < 800) {
-        document.getElementById('return_mobilePopUp').classList.remove('displayNone');
-        document.getElementById('return_editMobilePopUp').classList.remove('displayNone');
-        document.getElementById('mobileContactReturn').classList.remove('displayNone');
-    }
-}
+// function hideMobileAssets() {// 
+//     if (window.innerWidth > 800) {
+//         document.getElementById('mobileContactReturn').classList.add('displayNone');
+//         document.getElementById('return_mobilePopUp').classList.add('displayNone');
+//         document.getElementById('return_editMobilePopUp').classList.add('displayNone');
+//     } else if (window.innerWidth < 800) {
+//         document.getElementById('return_mobilePopUp').classList.remove('displayNone');
+//         document.getElementById('return_editMobilePopUp').classList.remove('displayNone');
+//         document.getElementById('mobileContactReturn').classList.remove('displayNone');
+//     }
+// }
 
 function showContactMobile() {
     document.getElementById('contentSection').classList.add('dNone');
